e88bc0413a44e34447afcd93579bb9de68411286
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-06-22 03:19:12 +0530

    hy

---
+++ assigment operator.py
@@ -0,0 +1,37 @@
+#assigment operator
+
+#sum
+num =  10 
+num = num + 10 #10 + 10
+num += 10 #same answer
+print("Answer num : ",num)
+
+#sub
+a = 30
+a = a - 10 
+a -= 10 #same work krega upper wali line ka 
+print("Answer a = ",a)
+
+
+# multi
+b = 30
+b = b * 90
+b *= 5 #same work krega upper wali line ka 
+print(" Answer b = ", b)
+
+
+
+#divide
+divide = 90
+divide = divide/5
+divide /= 2  #same work upper wali line ka 
+print("Answer Divide = ",divide)
+
+
+
+# power opertro (root operator)
+poweroperator = 90
+poweroperator = poweroperator ** 10
+poweroperator **= 80 #same work krega upper wali line ka 
+print("Answer power operator = ",poweroperator)
+
---
+++ logical operator.py
@@ -0,0 +1,31 @@
+"""
+logical operator 
+there are three type 
+1. not
+2. and
+3. or
+
+"""
+# not logical operator
+#  means correct answer provide to  opposite answer
+
+print(not False) #output is true (because not operator given answer is opposite)
+print(not True) #output is False
+ 
+a = 90
+b = 80
+print(not(a>b)) #output is false
+print(not(a<b)) #output is True
+
+
+#And opeator
+val1 = True
+val2 = False
+print("And oprator :",val1 and val2)
+
+
+
+#OR operator 
+val1 = False
+val2 = False
+print("OR operator :",val1 or val2)
