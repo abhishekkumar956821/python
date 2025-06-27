fa3c5d7e3e6abbd5f1db186826b3494a7f9a660a
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-06-27 19:24:00 +0530

    hy

---
+++ input 2 ing num print true a is grater than.py
@@ -0,0 +1,10 @@
+#WAP to input 2 int number, a and b.
+#print True if a is greater than or equal to b. if not print False.
+
+a = int(input("Enter the first value :"))
+b = int(input("Enter the second value :"))
+
+greatherthen = a >= b
+print("your Answer :",greatherthen)
+
+
