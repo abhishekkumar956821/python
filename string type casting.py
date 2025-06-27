ad844481f7fba349d50d955b240de6356d9533c1
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-06-27 07:13:46 +0530

    hy

---
+++ string type casting.py
@@ -0,0 +1,8 @@
+#string type casting
+
+a = 3.45
+a = str(a)
+
+print(type(a))  
+
+#output = "str"
---
+++ type casting.py
@@ -0,0 +1,9 @@
+ # type casting  haam type casting ko use krta h jab dusre data type ko same bnana ho to type casting ka use krta h 
+
+
+
+a = int("2")
+b = 4.79
+
+print(type(a))
+print(a+b)
