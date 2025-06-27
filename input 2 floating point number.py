3b425cfd2192ec6ab5ae54dc1cd81c3a16acec8b
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-06-27 19:16:29 +0530

    hy

---
+++ input 2 floating point number.py
@@ -0,0 +1,8 @@
+#WAP to input 2 floating point number & print their average.
+
+a = float(input("Enter first number :"))
+b = float(input("Enter second number :"))
+
+avg = (a+b)/2
+
+print("Average value ",avg)
---
+++ input side of a squre.py
@@ -0,0 +1,5 @@
+ # WAP to input side of a squre & print its area
+
+side = float(input("Enter the squre side "))
+Area = side * side
+print("Area: ", Area)
