fe1d0d4604bcb698623bbaf1820f0ac94e904fca
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-06-27 19:04:12 +0530

    hy

---
+++ input two number and sum.py
@@ -0,0 +1,8 @@
+#write a program to input 2 number & print their sum
+
+first = input("Enter the first number :")
+second = input("Enter the second number :")
+
+sum = first + second 
+
+print("Enter total value",sum)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,8 @@
 a = int(input("Enter your age :" ))
-# print("Your age is ", a)
+print("Your age is ", a)
 
-# if(a>18):
-#     print("you can drive")
-# else:
-#     print("you cannot drive")
+if(a>18):
+    print("you can drive")
+else:
+    print("you cannot drive")
     
