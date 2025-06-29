abf1760523f125bbb3d7e4a5f80b6b7cf6d5df66
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-06-30 00:32:06 +0530

    hy

--- string code.py
+++ string code.py
@@ -1,1 +1,10 @@
-str = " this is a string (using double coat using like = "")"
+str1 = " this is a string (using double coat using like = "")\n"
+str2= 'this is a string (using double single using like = '')\n'
+str3 = """this is a string (using double triple coat like = """""")\n"""
+
+print(str1, str2, str3)
+
+
+# When we write any long sentence and want to break it into a new line in between,
+#  we use \n to insert the line break like ="\n"
+#Look up what I did for the new line.
