bcccabb488699948c259f41571bad9f71efac2c7
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-06-30 00:37:01 +0530

    hy

--- string code.py
+++ string code.py
@@ -1,10 +1,11 @@
 str1 = " this is a string (using double coat using like = "")\n"
 str2= 'this is a string (using double single using like = '')\n'
 str3 = """this is a string (using double triple coat like = """""")\n"""
-
+str4 = "this is a string. \t we are creating it in python " 
 print(str1, str2, str3)
 
 
 # When we write any long sentence and want to break it into a new line in between,
 #  we use \n to insert the line break like ="\n"
 #Look up what I did for the new line.
+# aagar ham tab ka space use krna h to uske liya = "\t" use krta h 
