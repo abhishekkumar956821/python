9248b0212a0248e7edfe0f7de10d0427df9605c5
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-07-03 07:30:00 +0530

    hy

--- length of string.py
+++ length of string.py
@@ -6,6 +6,6 @@ str2 = "kumar"
 len2 = len(str2)
 print("length is :",len2)
 print(str1 + str2)
-final_str = str1 + " " + str2 #agar length ke ander kuch bo like space bi ho sab count hota h 
+final_str = str1 + " " + str2 #agar length ke ander kuch bi like space bi ho sab count hota h 
 final_len = len(final_str)
 print(len(final_str))
