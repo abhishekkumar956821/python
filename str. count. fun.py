65cf07064d42933d36b57661bdbe60381f3734e0
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-07-04 19:36:42 +0530

    hy

---
+++ str. count. fun.py
@@ -0,0 +1,17 @@
+# string m count function ka mtlb hota h ki string me koi bi value kitni bar aai h uski value print hoti h 
+
+str = "i am a student of b.tech and i am a full stack developer "
+
+print(str.count("of")) #yha pr of ki value count hoke print hogi mtlb ki of kiti baar aaya h 
+
+print(str.capitalize()) #yha pr string ki value ko captial kr dega 
+
+
+print(str.endswith("developer")) # yha pr ending ki value ko end me developer h ya nhi ki check krega or print krega true of false
+
+print(str.startswith("i am ")) # yha pr starting ki value ko start me i am h ya nhi check krega or print krega true of false 
+
+print(str.isalnum()) # yha pr check krega ki starting me koi alphanumeric value h ya nhi mtlb ki string me koi number or letter dono h ya nhi or print krega true or false
+
+print(str.isalpha()) # yha pr check krega ki string me koi alphabetic value h ya nhi mtlb ki string me sirf letter h or koi number nhi h or print krega true or false
+
