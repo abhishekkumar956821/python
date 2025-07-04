d6c3e39cc0fc27bc7ab2c5cb9e7c2b8a60f335fd
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-07-04 19:19:41 +0530

    hy

---
+++ str. find fun.py
@@ -0,0 +1,5 @@
+#str.find("") ka mtlb hota h ki kisi string me kisi value ko search krna usnki index value output hoke ati h 
+
+str = "i am a student of b.tech and i am a fullstack developer"
+
+print(str.find("student")) #yha pr student ka index print hoga 
