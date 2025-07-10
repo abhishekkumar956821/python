36ab52f0440dff76b19eb4fdbbfb06746f42e3b8
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-07-10 21:21:30 +0530

    hyy

---
+++ list apned method.py
@@ -0,0 +1,6 @@
+#list.append(value) # This method adds an element at the end of the list mtlb ki ye method list ke end mtlb last m element ko add krta h
+
+list = [2, 1, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Original List:", list)
+list.append(11) # Adding 11 at the end of the list
+print("List after appending 11:", list) 
---
+++ list methods.py
@@ -0,0 +1,24 @@
+list = [2, 1, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Original List:", list)
+list.reverse() # Reversing the list
+print("Reversed List:", list)
+
+
+
+#list = [2,1,3,4,5,6,7,8,9,10]
+#list.append(11) #adding 11 at the end of list
+#list.insert(0, 0) #adding 0 at the start of list
+#list.remove(5) #removing 5 from the list
+#list.pop() #removing last element from the list
+#list.sort() #sorting the list in ascending order
+#list.reverse() #reversing the list
+#list.clear() #clearing the list
+#list.sort(reverse=True) #sorting the list in descending order
+#list.insert(idx, element) #inserting element at index idx
+#list.remove(element) #removing element from the list
+#list.pop(idx) #removing element at index idx
+#list.index(element) #returning index of element in the list
+#list.count(element) #returning count of element in the list
+#list.copy() #returning copy of the list    
+#list.extend(iterable) #adding elements of iterable to the end of the list
+#list.clear() #clearing the list
