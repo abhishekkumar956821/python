55c32207e5d66c25fc2e84e3a118f6ba79ba959b
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-07-10 09:13:49 +0530

    hyy

---
+++ list.py
@@ -0,0 +1,14 @@
+#List in python
+#A built-in data type that stores set of values
+#it can store elements of different types (integers,float,strings,etc.)
+
+
+marks = [19, 59.8, 78, 45, 78, 22]
+print("Marks :",marks)
+print("length of list :",len(marks))
+print("Type of marks :",type(marks))
+print("First element of list :",marks[0])
+
+print("Last element of list :",marks[-1])
+print("First 3 elements of list :",marks[0:3])
+print("Last 3 elements of list :",marks[-3:])   
