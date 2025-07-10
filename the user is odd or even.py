7d073ea6a8b24f808dbcd4a8301559a784d77847
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-07-10 08:39:15 +0530

    hyy

---
+++ the user is odd or even.py
@@ -0,0 +1,8 @@
+# WAP to check if a numbber entered by the user is odd or even
+
+num = int(input("Enter a Number: "))
+
+if(num %2 ==0):
+    print("The Number is Even")
+else:
+    print("The Number is Odd")
