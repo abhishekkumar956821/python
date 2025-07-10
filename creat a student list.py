4212ca845c1913d616bf50ce6d28a0c3cbbafe15
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-07-10 09:21:57 +0530

    hyy

---
+++ creat a student list.py
@@ -0,0 +1,10 @@
+#if i create a student list then i can add the student name and roll number in it
+#string => immutable mtlb ki jo caange nhi ho sakti h 
+#List => mutable mtlb ki jo change ho sakti h 
+
+student = ["Lavish", "Rohit", "sanju"]
+
+roll_number = [101, 102, 103]
+
+print("Student Name: ",student)
+print("Student Roll Number: ",roll_number)
