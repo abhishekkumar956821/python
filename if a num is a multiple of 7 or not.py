c0090d1c2b4a3c0a2698ce3f281d64b09e0b2352
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-07-10 08:56:36 +0530

    hyy

---
+++ if a num is a multiple of 7 or not.py
@@ -0,0 +1,8 @@
+#WAP to check if a number is a Multiple of & or not.
+
+num = int(input("Enter the number: "))
+
+if(num%7 == 0):
+    print("The number is a multiple of 7")
+else:
+    print("The number is not a multiple of 7")
