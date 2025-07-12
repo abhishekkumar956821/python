cad429d86ca76b60f95c4970d07d56d5caecc5b1
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-07-12 17:43:15 +0530

    hyy

---
+++ list short reverse.py
@@ -0,0 +1,11 @@
+#list.sort reverse method
+#list.sort(reverse=True) # This method sorts the list in descending order
+#agar haam list.sort(reverse=True) 
+# use krte h to ye list ko descending order mtlb badi se choti ki taraf sort krta h
+
+
+list = [2, 1, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Original List:", list)
+list.sort(reverse=True)
+
+print("List after sorting in descending order:", list)  # This will sort the list in descending order
