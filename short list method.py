d0c96c52bcce05f510fdbf2ef714a9ca17954b14
abhishekkumr956821 <abhishekkumar956821@gmail.com>
2025-07-12 17:39:32 +0530

    hyy

---
+++ short list method.py
@@ -0,0 +1,7 @@
+#shorting list method
+#shorting m ascending order mtlb ki ye method list ko choti se badi ki taraf sort krta h
+#list.sort() # This method sorts the list in ascending order
+list = [2, 1, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Original List:", list)
+list.sort()
+print("List after sorting:", list)  # This will sort the list in ascending order
