c544e8c722021f4127f606a0e6cafdc074405294
abhishekkumar956821 <abhishekkumar956821@gmail.com>
2025-07-15 15:35:43 +0530

    hyy

---
+++ Enter names of their 3 fav. movie.py
@@ -0,0 +1,13 @@
+#WAP to ask the user to enter name of their 3 favorite movies & store them in a list
+
+movies = []
+
+movie1 = input("Enter the 1st movie")
+movie2 = input("Enter the 2nd movie ")
+movie3 = input("Enter the 3rd movie")
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+
+print(movies)
