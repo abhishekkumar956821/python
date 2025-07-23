d45736956e0a7c6305b53dfc6874957cd63d9d87
abhishekkumar956821 <abhishekkumar956821@gmail.com>
2025-07-23 23:07:12 +0530

    hyy

--- lec4.1 dictionary key value.py
+++ lec4.1 dictionary key value.py
@@ -1,10 +1,25 @@
 info = {
     "key" : "value",
     "name" : "Abhi",
+    "sub" : ["python","HTML","CSS"],
     "learning" : "coding",
     "age" : 21,
     "is_adult" : True,
-    
+    12 : 23
 }
 
-print(info)
+print(type(info))
+print(info["key"])
+print(info["name"])
+
+# Agar hamko key value add krni h to haam kr sakta h 
+
+info["name"] = "Abhishek kumar"
+info["age"] = "23"
+
+
+# haam aalag se null_dict bi add kr sakta h 
+
+null_dict = {}
+null_dict["dob"] = "2004"
+print(null_dict)
---
+++ lec4.2 nested dict.py
@@ -0,0 +1,33 @@
+# Nested function to create a directory
+
+student = {
+    "name": "John Doe",
+    "subjects": {
+        "Math": 90,
+        "English": 85,
+        "Science": 95,
+        "Social Studies": 88,
+        "Computer Science": 92,
+        "Physics": 87,
+        "Chemistry": 93,
+        "Biology": 90,
+        "History": 91,
+        "Geography": 89,
+        "French": 86,
+        "German": 94,
+        "Italian": 88,
+        "Spanish": 92,
+        "Arabic": 90,
+        "Chinese": 87,
+        "Japanese": 95,
+        "Russian": 93,
+        "Turkish": 89,
+        "Portuguese": 91,
+        "Swedish": 88,
+        "Dutch": 94,
+    },
+    "roll_number": 101
+}
+print("Student Name : ", student["name"])
+print("Student Subjects : ", student["subjects"])
+print(type(student))
