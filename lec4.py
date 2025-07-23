2d5684d9ac818ac6a8a2e65356483d44710360b6
abhishekkumar956821 <abhishekkumar956821@gmail.com>
2025-07-23 21:48:05 +0530

    hyy

---
+++ lec4.py
@@ -0,0 +1,13 @@
+# #Dictionary in python 
+
+# Dictionaries are used to store data values in key:value pairs 
+# They are unordered, mutable(changeable) & don't allow duplicate keys
+
+# dict = {
+#     "name" : "abhi"
+#     "cgpa" : 9.6,
+#     "marks" : [98,97,95],
+# }
+# dict["name"], dict["cgpa"],dict["marks"]
+# dict["key"] = "value" #to assign or add new
+
